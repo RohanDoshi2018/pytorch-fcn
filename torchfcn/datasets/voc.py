fe1ce73dd6dd7af670d7321592c1c75c016d7b96
Rohan K. Doshi <rkdoshi@visualai-3.cs.princeton.edu>
2018-01-08 16:34:07 -0500

    implemented nearest embedding vector version - not working

--- torchfcn/datasets/voc.py
+++ torchfcn/datasets/voc.py
@@ -8,10 +8,14 @@ import PIL.Image
 import scipy.io
 import torch
 from torch.utils import data
+import pickle
 
+# utility function for loading files with pickeled objects
+def load_obj(path, name):
+    with open(path + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
 class VOCClassSegBase(data.Dataset):
-
     class_names = np.array([
         'background',
         'aeroplane',
@@ -37,28 +41,32 @@ class VOCClassSegBase(data.Dataset):
     ])
     mean_bgr = np.array([104.00698793, 116.66876762, 122.67891434])
 
-    def __init__(self, root, split='train', transform=False):
+    def __init__(self, root, split='train', transform=False, pixel_embeddings=None):
         self.root = root
         self.split = split
         self._transform = transform
+        self.pixel_embeddings = pixel_embeddings
+
+        if self.pixel_embeddings:
+            embed_dim = pixel_embeddings # dimensions in each embedding
+            embeddings_dict = load_obj('/opt/visualai/rkdoshi/pytorch-fcn/examples/voc/', 'label2vec_dict_' + str(embed_dim))
+            num_embed = embeddings_dict.shape[0] #  21 = background (class 0) + labels (class 1-20)
+            self.embeddings = torch.nn.Embedding(num_embed, embed_dim)
+            self.embeddings.weight.requires_grad = False
+            self.embeddings.weight.data.copy_(torch.from_numpy(embeddings_dict))
 
         # VOC2011 and others are subset of VOC2012
         dataset_dir = osp.join(self.root, 'VOC/VOCdevkit/VOC2012')
         self.files = collections.defaultdict(list)
         for split in ['train', 'val']:
-            imgsets_file = osp.join(
-                dataset_dir, 'ImageSets/Segmentation/%s.txt' % split)
+            imgsets_file = osp.join(dataset_dir, 'ImageSets/Segmentation/%s.txt' % split)
             for did in open(imgsets_file):
                 did = did.strip()
                 img_file = osp.join(dataset_dir, 'JPEGImages/%s.jpg' % did)
-                lbl_file = osp.join(
-                    dataset_dir, 'SegmentationClass/%s.png' % did)
-                self.files[split].append({
-                    'img': img_file,
-                    'lbl': lbl_file,
-                })
+                lbl_file = osp.join(dataset_dir, 'SegmentationClass/%s.png' % did)
+                self.files[split].append({'img': img_file, 'lbl': lbl_file,})
 
-    def __len__(self):
+    def __len__(self):          
         return len(self.files[self.split])
 
     def __getitem__(self, index):
@@ -72,8 +80,17 @@ class VOCClassSegBase(data.Dataset):
         lbl = PIL.Image.open(lbl_file)
         lbl = np.array(lbl, dtype=np.int32)
         lbl[lbl == 255] = -1
+        if self.pixel_embeddings:
+            # change to  0 instead of -1 because embedding lookup cannot handle index -1
+            lbl_cpy = np.copy(lbl)
+            lbl_cpy[lbl_cpy == -1] = 0 
+            lbl_vec = self.embeddings(torch.from_numpy(lbl_cpy).long()).data
+
         if self._transform:
-            return self.transform(img, lbl)
+            img, lbl = self.transform(img, lbl)
+                
+        if self.pixel_embeddings:
+            return img, (lbl, lbl_vec)
         else:
             return img, lbl
 
@@ -98,13 +115,13 @@ class VOCClassSegBase(data.Dataset):
 
 class VOC2011ClassSeg(VOCClassSegBase):
 
-    def __init__(self, root, split='train', transform=False):
+    def __init__(self, root, split='train', transform=False, pixel_embeddings=None):
         super(VOC2011ClassSeg, self).__init__(
-            root, split=split, transform=transform)
+            root, split=split, transform=transform, pixel_embeddings=pixel_embeddings)
+        if self.pixel_embeddings:
+            embeddings_dict = load_obj('/opt/visualai/rkdoshi/pytorch-fcn/examples/voc/', 'label2vec_dict_' + str(pixel_embeddings))
         pkg_root = osp.join(osp.dirname(osp.realpath(__file__)), '..')
-        imgsets_file = osp.join(
-            pkg_root, 'ext/fcn.berkeleyvision.org',
-            'data/pascal/seg11valid.txt')
+        imgsets_file = osp.join(pkg_root, 'ext/fcn.berkeleyvision.org', 'data/pascal/seg11valid.txt')
         dataset_dir = osp.join(self.root, 'VOC/VOCdevkit/VOC2012')
         for did in open(imgsets_file):
             did = did.strip()
@@ -118,8 +135,7 @@ class VOC2012ClassSeg(VOCClassSegBase):
     url = 'http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar'  # NOQA
 
     def __init__(self, root, split='train', transform=False):
-        super(VOC2012ClassSeg, self).__init__(
-            root, split=split, transform=transform)
+        super(VOC2012ClassSeg, self).__init__(root, split=split, transform=transform)
 
 
 class SBDClassSeg(VOCClassSegBase):
@@ -127,10 +143,19 @@ class SBDClassSeg(VOCClassSegBase):
     # XXX: It must be renamed to benchmark.tar to be extracted.
     url = 'http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz'  # NOQA
 
-    def __init__(self, root, split='train', transform=False):
+    # pixel_embeddings is set to the number of dimensions for the pixel-embeddings
+    def __init__(self, root, split='train', transform=False, pixel_embeddings=None):
         self.root = root
         self.split = split
         self._transform = transform
+        self.pixel_embeddings = pixel_embeddings
+        if self.pixel_embeddings:   
+            embed_dim = pixel_embeddings # dimensions in each embedding
+            embeddings_dict = load_obj('/opt/visualai/rkdoshi/pytorch-fcn/examples/voc/', 'label2vec_dict_' + str(embed_dim))
+            num_embed = embeddings_dict.shape[0] #  21 = background (class 0) + labels (class 1-20)
+            self.embeddings = torch.nn.Embedding(num_embed, embed_dim)
+            self.embeddings.weight.requires_grad = False
+            self.embeddings.weight.data.copy_(torch.from_numpy(embeddings_dict))
 
         dataset_dir = osp.join(self.root, 'VOC/benchmark_RELEASE/dataset')
         self.files = collections.defaultdict(list)
@@ -140,10 +165,7 @@ class SBDClassSeg(VOCClassSegBase):
                 did = did.strip()
                 img_file = osp.join(dataset_dir, 'img/%s.jpg' % did)
                 lbl_file = osp.join(dataset_dir, 'cls/%s.mat' % did)
-                self.files[split].append({
-                    'img': img_file,
-                    'lbl': lbl_file,
-                })
+                self.files[split].append({'img': img_file, 'lbl': lbl_file,})
 
     def __getitem__(self, index):
         data_file = self.files[self.split][index]
@@ -154,9 +176,18 @@ class SBDClassSeg(VOCClassSegBase):
         # load label
         lbl_file = data_file['lbl']
         mat = scipy.io.loadmat(lbl_file)
-        lbl = mat['GTcls'][0]['Segmentation'][0].astype(np.int32)
+        lbl = mat['GTcls'][0]['Segmentation'][0].astype(np.int32)   
         lbl[lbl == 255] = -1
+
+        if self.pixel_embeddings:
+            lbl_cpy = np.copy(lbl)
+            lbl_cpy[lbl_cpy == -1] = 0
+            lbl_vec = self.embeddings(torch.from_numpy(lbl_cpy).long()).data
+        
         if self._transform:
-            return self.transform(img, lbl)
+            img, lbl = self.transform(img, lbl)
+        
+        if self.pixel_embeddings:
+            return img, (lbl, lbl_vec)
         else:
             return img, lbl
--- torchfcn/trainer.py
+++ torchfcn/trainer.py
@@ -3,7 +3,6 @@ import math
 import os
 import os.path as osp
 import shutil
-
 import fcn
 import numpy as np
 import pytz
@@ -12,9 +11,8 @@ import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
 import tqdm
-
+import pickle
 import torchfcn
-
 
 def cross_entropy2d(input, target, weight=None, size_average=True):
     # input: (n, c, h, w), target: (n, h, w)
@@ -33,23 +31,71 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
         loss /= mask.data.sum()
     return loss
 
+# mean square error between two vectors (score and target)
+# args:
+#      score -> torch.Size([1, 50, 366, 500])
+#      target -> torch.Size([1, 366, 500, 50])
+# return: 
+#     loss -> scalar
+def mse_embedding(score, target, size_average=True):
+    n, c, h, w = score.size()
+
+    # target: torch.Size([1, 366, 500, 50]) -> should be torch.Size([1, 50, 366, 500])
+    target = target.permute(0,3,1,2)
+
+    # create n x h x w mask with 1's for observed classes, 0 for background using target as ground truth
+    mask = torch.sum(target, dim=1) # sum along channel (should be all zeros for background)
+    mask[mask != 0] = 1
+    mask[mask == 0] = 0
+
+    # apply mask to score and target, and turn into 1d vectors for comparision
+    mask_tensor = mask.view(1,1,h,w).repeat(1,c,1,1)
+    score_mask = score[mask_tensor==1]
+    target_mask = target[mask_tensor==1]
+
+    # calculate loss on masked score and target
+    loss = F.mse_loss(score_mask, target_mask, size_average=False)
+
+    if size_average:
+        loss /= mask.data.sum()
+
+    return loss
+
+# get nearest label prediction for pixel embeddings of size (n,c, h, w) 
+# score: torch.Size([1, 50, 366, 500])
+def get_lbl_pred(score, embed_arr):
+   n, c, h, w = score.size()
+   embeddings = embed_arr.transpose(1,0).repeat(1,h*w,1,1)
+   score = score.view(1,h*w,c,1).repeat(1,1,1,20)
+   dist = score.data - embeddings
+   dist = dist.pow(2).sum(2).sqrt()
+   min_val, indices = dist.min(2)
+   indices = indices + 1
+   return indices.view(1,h,w).cpu().numpy()
+
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
 class Trainer(object):
 
     def __init__(self, cuda, model, optimizer,
-                 train_loader, val_loader, out, max_iter,
-                 size_average=False, interval_validate=None):
+                    train_loader, val_loader, out, max_iter,
+                    size_average=False, interval_validate=None,
+                    pixel_embeddings=None):
         self.cuda = cuda
-
         self.model = model
         self.optim = optimizer
-
+        self.pixel_embeddings = pixel_embeddings
         self.train_loader = train_loader
         self.val_loader = val_loader
-
-        self.timestamp_start = \
-            datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+        self.timestamp_start = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
         self.size_average = size_average
+
+        if self.pixel_embeddings:
+           self.embeddings = load_obj('/opt/visualai/rkdoshi/pytorch-fcn/examples/voc/label2vec_dict_' + str(pixel_embeddings))
+           self.embeddings = self.embeddings[1:,:]
+           self.embeddings = torch.from_numpy(self.embeddings).cuda().float()
 
         if interval_validate is None:
             self.interval_validate = len(self.train_loader)
@@ -78,13 +124,13 @@ class Trainer(object):
         if not osp.exists(osp.join(self.out, 'log.csv')):
             with open(osp.join(self.out, 'log.csv'), 'w') as f:
                 f.write(','.join(self.log_headers) + '\n')
-
         self.epoch = 0
         self.iteration = 0
         self.max_iter = max_iter
         self.best_mean_iu = 0
 
     def validate(self):
+        training = self.model.training
         self.model.eval()
 
         n_class = len(self.val_loader.dataset.class_names)
@@ -93,23 +139,40 @@ class Trainer(object):
         visualizations = []
         label_trues, label_preds = [], []
         for batch_idx, (data, target) in tqdm.tqdm(
-                enumerate(self.val_loader), total=len(self.val_loader),
-                desc='Valid iteration=%d' % self.iteration, ncols=80,
-                leave=False):
+                        enumerate(self.val_loader), total=len(self.val_loader),
+                        desc='Valid iteration=%d' % self.iteration, ncols=80,
+                        leave=False):
+
+            if self.pixel_embeddings:
+                target, target_embed = target
+            
             if self.cuda:
                 data, target = data.cuda(), target.cuda()
             data, target = Variable(data, volatile=True), Variable(target)
-            score = self.model(data)
 
-            loss = cross_entropy2d(score, target,
-                                   size_average=self.size_average)
+            if self.pixel_embeddings:
+                target_embed = target_embed.cuda()
+                target_embed = Variable(target_embed)
+
+            score = self.model(data)
+            if self.pixel_embeddings:
+                loss = mse_embedding(score, target_embed, size_average=self.size_average)
+            else:
+                loss = cross_entropy2d(score, target, size_average=self.size_average)
+           
             if np.isnan(float(loss.data[0])):
+                # continue
+                # raise Exception(loss, score, target, target_embed)
                 raise ValueError('loss is nan while validating')
             val_loss += float(loss.data[0]) / len(data)
 
             imgs = data.data.cpu()
-            lbl_pred = score.data.max(1)[1].cpu().numpy()[:, :, :]
+            if self.pixel_embeddings:	
+                lbl_pred = get_lbl_pred(score, self.embeddings)
+            else:
+                lbl_pred = score.data.max(1)[1].cpu().numpy()[:, :, :]
             lbl_true = target.data.cpu()
+
             for img, lt, lp in zip(imgs, lbl_true, lbl_pred):
                 img, lt = self.val_loader.dataset.untransform(img, lt)
                 label_trues.append(lt)
@@ -118,9 +181,8 @@ class Trainer(object):
                     viz = fcn.utils.visualize_segmentation(
                         lbl_pred=lp, lbl_true=lt, img=img, n_class=n_class)
                     visualizations.append(viz)
-        metrics = torchfcn.utils.label_accuracy_score(
-            label_trues, label_preds, n_class)
 
+        metrics = torchfcn.utils.label_accuracy_score(label_trues, label_preds, n_class)
         out = osp.join(self.out, 'visualization_viz')
         if not osp.exists(out):
             os.makedirs(out)
@@ -151,8 +213,10 @@ class Trainer(object):
             'best_mean_iu': self.best_mean_iu,
         }, osp.join(self.out, 'checkpoint.pth.tar'))
         if is_best:
-            shutil.copy(osp.join(self.out, 'checkpoint.pth.tar'),
-                        osp.join(self.out, 'model_best.pth.tar'))
+            shutil.copy(osp.join(self.out, 'checkpoint.pth.tar'), osp.join(self.out, 'model_best.pth.tar'))
+        
+        if training:
+            self.model.train()
 
     def train_epoch(self):
         self.model.train()
@@ -170,22 +234,39 @@ class Trainer(object):
             if self.iteration % self.interval_validate == 0:
                 self.validate()
 
+            assert self.model.training
+
+            if self.pixel_embeddings:
+                 target, target_embed = target
             if self.cuda:
-                data, target = data.cuda(), target.cuda()
+                 data, target = data.cuda(), target.cuda()
             data, target = Variable(data), Variable(target)
+
+            if self.pixel_embeddings:
+                target_embed = target_embed.cuda()
+                target_embed = Variable(target_embed)
+
             self.optim.zero_grad()
             score = self.model(data)
 
-            loss = cross_entropy2d(score, target,
-                                   size_average=self.size_average)
+            if self.pixel_embeddings:
+                loss = mse_embedding(score, target_embed, size_average=self.size_average)
+            else:
+                loss = cross_entropy2d(score, target, size_average=self.size_average)
+
             loss /= len(data)
             if np.isnan(float(loss.data[0])):
+                # continue
+                # raise Exception(loss, score, target, target_embed)
                 raise ValueError('loss is nan while training')
             loss.backward()
             self.optim.step()
 
             metrics = []
-            lbl_pred = score.data.max(1)[1].cpu().numpy()[:, :, :]
+            if self.pixel_embeddings: 
+                lbl_pred = get_lbl_pred(score, self.embeddings)
+            else:
+                lbl_pred = score.data.max(1)[1].cpu().numpy()[:, :, :]
             lbl_true = target.data.cpu().numpy()
             for lt, lp in zip(lbl_true, lbl_pred):
                 acc, acc_cls, mean_iu, fwavacc = \
